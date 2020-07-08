40ea7bd9b13530c4b35be6c0a8557758d5c8629a
Gwen <itsgwenhey@gmail.com>
2020-07-07 22:42:09 -0500

    fix dumb spelling error that took me hours to fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
         :recoverable, :rememberable, :validatable
 
-  has_many :created_events, foreign_key: :owner_id, class_name: "Events"
+  has_many :created_events, foreign_key: :owner_id, class_name: "Event"
   has_many :attendees, through: :attendees
 
 end
